e215452d957f0a9e9ceca90559c18b257873a7e8
Saikat-Chowdhury90 <78464310+Saikat-Chowdhury90@users.noreply.github.com>
2021-09-20 19:00:01 +0530

    autofillskillbar

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ for(var i=0;i<anchortags.length;i++)
         event.preventDefault();
         var targetSectionID=this.textContent.trim().toLowerCase();
         var targetSection=document.getElementById(targetSectionID);
-        //console.log(targetSection);
+       // console.log(targetSection);
         
         var interval=setInterval(function() {
             var targetCoordinates=targetSection.getBoundingClientRect();
-            if(targetCoordinates.top<=0)
+            // console.log(targetCoordinates);
+            if(targetCoordinates.top<=0 )
             {
                 clearInterval(interval);
                 return;
@@ -23,3 +24,79 @@ for(var i=0;i<anchortags.length;i++)
 
     });
 }
+
+
+
+//skill-bar
+
+//handle scroll event on window
+
+//check that skill sections container is visisble or not
+//ensure that your initial skill was initialised zero
+//start animation on every skill => increase the skill width from 0 to skill level at regular intervals
+//Store skill level--> html atrtribute using data-attribute
+
+var progressbar=document.querySelectorAll(".skills-acquire > div");
+//console.log(progressbar);
+var skillcontainer=document.getElementById("skill-container");
+//console.log(skillcontainer);
+var expirience=document.getElementById("expirience");
+var count=0;
+var isanimation=false;
+window.addEventListener('scroll',checkscroll);
+function initialisingbars() {
+
+        for(let bar of progressbar)
+        {
+            bar.style.width=0+'%';
+        }
+    
+    
+   
+}
+initialisingbars();
+
+function fillbar() 
+{
+    for(let bar of progressbar)
+    {
+        let targetattribute=bar.getAttribute('data-attribute-width');
+        let currwidth=0;
+        let interval=setInterval(function(){
+            if(currwidth > targetattribute)
+            {
+                clearInterval(interval);
+
+                return;
+            }
+            currwidth++;
+            bar.style.width=currwidth+'%';
+        }, 8);
+        
+    }
+}
+function checkscroll() 
+{
+    //you have to check whether skill section is visible  
+    
+    var coordinates=skillcontainer.getBoundingClientRect();
+    
+    if(!isanimation  && coordinates.top<window.innerHeight)
+    {
+        isanimation=true;
+        count=0;
+        //console.log("skill-section-visible");
+        fillbar();
+    }
+   
+
+
+}
+
+
+
+
+
+
+
+
